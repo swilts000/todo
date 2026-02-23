95ef68e5f977d6d17962246b1e72f65dc82fda56
swilts000 <sharif_wiltshire@cable.comcast.com>
2026-02-22 19:58:17 -0500

    ok

---
+++ src/test/java/full_k8s_project/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package full_k8s_project.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
